3956763429733bdbbf20ff19c07b882f8a24d666
Sai Sumiran <164289250+Sumiran2105@users.noreply.github.com>
2025-11-17 12:54:43 +0530

    changes made in destination and layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from "./components/Layout";
+
 import Home from "./pages/Home";
 import TripDetails from "./pages/TripDetails";
 import BookingForm from "./pages/BookingForm";
@@ -9,9 +11,8 @@ import SuccessPage from "./pages/SuccessPage";
 
 import SignIn from "./components/SignIn";
 import SignUp from "./components/Signup";
-import Destinations from "./components/Destinations";
 
-// NEW IMPORTS
+import Destinations from "./components/Destinations";
 import CategoryPlaces from "./components/CategoryPlaces";
 import PlaceDetails from "./components/PlaceDetails";
 
@@ -19,23 +20,27 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Main Pages */}
-        <Route path="/" element={<Home />} />
-        <Route path="/destinations" element={<Destinations />} />
+        
+        {/* All pages inside Layout */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/destinations" element={<Destinations />} />
+          
+          {/* Categories Flow */}
+          <Route path="/category/:categoryId" element={<CategoryPlaces />} />
+          <Route path="/category/:categoryId/:placeId" element={<PlaceDetails />} />
+          
+          {/* Trip Flow */}
+          <Route path="/trip/:id" element={<TripDetails />} />
+          <Route path="/book/:id" element={<BookingForm />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/success" element={<SuccessPage />} />
+          
+          {/* Auth */}
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
 
-        {/* Auth */}
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-
-        {/* Trip Booking Flow */}
-        <Route path="/trip/:id" element={<TripDetails />} />
-        <Route path="/book/:id" element={<BookingForm />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/success" element={<SuccessPage />} />
-
-        {/* NEW: Category → Places → Details */}
-        <Route path="/category/:categoryId" element={<CategoryPlaces />} />
-        <Route path="/category/:categoryId/:placeId" element={<PlaceDetails />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import HomeBanner from "../components/Home/HomeBanner";
 import TripList from "../components/Home/TripList";
+import Ourservices from "../components/Home/Ourservices";
 
 export default function Home() {
   return (
@@ -12,9 +13,9 @@ export default function Home() {
         <div className="max-w-7xl mx-auto px-4 py-8">
           <HomeBanner />
           <TripList />
+          <Ourservices />
         </div>
       </main>
-      <Footer />
     </>
   );
 }
