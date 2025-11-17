2c556f8f945c707f73139e08ed940b5cce2bd400
RajeswarReddy560 <rajeswarreddy456@gmail.com>
2025-11-17 14:51:04 +0530

    cleaned

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+
 import HomeBanner from "../components/Home/HomeBanner";
 import TripList from "../components/Home/TripList";
-import Ourservices from "../components/Home/Ourservices";import Ourservices from "../components/Home/Ourservices";
+import Ourservices from "../components/Home/Ourservices";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      
       <main className="bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 py-8">
           <HomeBanner />
           <TripList />
           <Ourservices />
-          <Ourservices />
+        
         </div>
       </main>
     </>
